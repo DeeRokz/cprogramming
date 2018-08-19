52416b1340c390a3ac06c86b91fd121db31c28d3
DeeRokz <40665216+DeeRokz@users.noreply.github.com>
2018-08-19 12:51:09 +0530

    Update Quadratic_Eqn_Using_Structure.c

--- Quadratic_Eqn_Using_Structure.c
+++ Quadratic_Eqn_Using_Structure.c
@@ -6,24 +6,21 @@ struct equation
     
   float a,b,c,determinent,alfa1,beta1;
   
-};
+}eqan;
 
 int input()
- {
-  struct equation eqan;  
+{
   printf("Enter a, b, c respectively : ");
   scanf("%f%f%f",&eqan.a,&eqan.b,&eqan.c);
  }
   
 int compute()
- {
-  struct equation eqan;
+{
   eqan.determinent=eqan.b*eqan.b-4*eqan.a*eqan.c;
  }
   
 int output()
  {
-  struct equation eqan;
   if(eqan.determinent==0)
     printf("\n ALPHA=BETA=%f",-eqan.b/(2*eqan.a));
   else if(eqan.determinent<0)
