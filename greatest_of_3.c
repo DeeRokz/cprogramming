d1ed34feaf72f4960798eeeb70c0341aa69c7204
DeeRokz <40665216+DeeRokz@users.noreply.github.com>
2018-08-30 20:42:39 +0530

    Updated - Now Only main calls all the function Ie Input(), Greatest of 3(), Output();

--- greatest_of_3.c
+++ greatest_of_3.c
@@ -1,24 +1,30 @@
 #include<stdio.h>
 
-void greatest_number(int num1, int num2, int num3)
+void greatest_number(int num1, int num2, int num3, int *big)
 {
  if(num1>num2&&num1>num3)
-  printf("The Greatest Number Among Three Is : %d \n", num1);
+  *big=num1;
  else if(num2>num1&&num2>num3)
-  printf("The Greatest Number Among Three Is : %d \n", num2);
+  *big=num2;
  else
-  printf("The Greatest Number Among Three Is : %d \n", num3);
+  *big=num3;
 }
 
-void input(int num1,int num2,int num3)
+void input(int *num1,int *num2,int *num3)
 {
  printf("Enter three numbers : ");
- scanf("%d %d %d", &num1, &num2, &num3);
- greatest_number(num1, num2, num3);
+ scanf("%d %d %d", num1, num2, num3);
+}
+
+void output(int big)
+{
+ printf("The Greatest Number Among Three Is : %d \n", big);
 }
 
 void main()
 {
- int num1, num2, num3;
- input(num1, num2, num3);
+ int num1, num2, num3, big;
+ input(&num1, &num2, &num3);
+ greatest_number(num1, num2, num3, &big);
+ output(big);
 }
