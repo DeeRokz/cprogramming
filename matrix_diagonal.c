6deaa30c435db833aaa4a58a4ed6c0195c684916
DeeRokz <40665216+DeeRokz@users.noreply.github.com>
2018-08-31 18:48:15 +0530

    Updated Now array is dinamic

--- matrix_diagonal.c
+++ matrix_diagonal.c
@@ -1,29 +1,36 @@
 #include<stdio.h>
 
-void input(int a[][10], int *rows, int *columns)
+void input_dimentions(int *rows, int *columns)
 {
  printf("Enter the no of rows : ");
  scanf("%d", rows);
  printf("Enter the no of columns : ");
  scanf("%d", columns);
+}
+
+void input(int rows, int columns, int a[rows][columns])
+{
  
- printf("Enter %d elements : ", (*rows)*(*columns));
- for(int i=0; i<*rows; i++)
-  for(int j=0; j<*columns; j++)
+ printf("Enter %d elements : ", rows*columns);
+ for(int i=0; i<rows; i++)
+  for(int j=0; j<columns; j++)
    scanf("%d", &a[i][j]);
 }
 
-void output(int a[][10], int rows, int column)
+void output(int rows, int columns, int a[rows][columns])
 {
  printf("Diagonal Elements \n");
  for(int i=0; i<rows; i++)
-  for(int j=0; j<column; j++)
+  for(int j=0; j<columns; j++)
    if(i==j)
     printf("%d, ", a[i][j]);
 }
 
 void main()
 {
- int a[10][10], rows, columns;
- input(a, &rows, &columns); 
+ int rows, columns;
+ input_dimentions(&rows, &columns);
+ int a[rows][columns];
+ input(rows, columns, a);
+ output(rows, columns, a);
 }
